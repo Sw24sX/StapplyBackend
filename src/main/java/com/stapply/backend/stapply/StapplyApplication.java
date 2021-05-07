61f1c921029830e9595cc532bd6f9fb4c5c9a6e3
Sw24sX <aleksandr.korolyov.99@mail.ru>
2021-05-07 15:18:57 +0500

    first

---
+++ src/main/java/com/stapply/backend/stapply/StapplyApplication.java
@@ -0,0 +1,13 @@
+package com.stapply.backend.stapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StapplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StapplyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stapply/backend/stapply/StapplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stapply.backend.stapply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StapplyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
