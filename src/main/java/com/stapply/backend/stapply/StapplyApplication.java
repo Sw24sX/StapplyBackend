eaf31b4936f363fa01d5851ee7636ba8cb9dbfe5
Sw24sX <aleksandr.korolyov.99@mail.ru>
2021-05-07 23:10:32 +0500

    add swagger

--- src/main/java/com/stapply/backend/stapply/StapplyApplication.java
+++ src/main/java/com/stapply/backend/stapply/StapplyApplication.java
@@ -2,8 +2,10 @@ package com.stapply.backend.stapply;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class StapplyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/stapply/backend/stapply/controller/AppMainController.java
+++ src/main/java/com/stapply/backend/stapply/controller/AppMainController.java
@@ -2,6 +2,7 @@ package com.stapply.backend.stapply.controller;
 
 import com.stapply.backend.stapply.models.AppMain;
 import com.stapply.backend.stapply.service.AppMainService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +11,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/apps")
 public class AppMainController {
-    @Autowired
-    private AppMainService appService;
+    private final AppMainService appService;
 
-    @GetMapping("/apps")
+    @Autowired
+    public AppMainController(AppMainService appService) {
+        this.appService = appService;
+    }
+
+    @GetMapping
     public ResponseEntity<List<AppMain>> getAllAppsMain() {
         final var result = appService.findAll();
         return result != null && !result.isEmpty() ?
@@ -22,7 +28,7 @@ public class AppMainController {
                 new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/apps/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<?> getAppMain(@PathVariable(name = "id")Long id) {
         final var result = appService.findById(id);
         return result != null ?
@@ -30,7 +36,7 @@ public class AppMainController {
                 new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/apps/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<?> changeName(@PathVariable(name = "id")Long id, @RequestBody AppMain appWithNewName) {
         final var app = appService.findById(id);
         if(app == null)
@@ -43,7 +49,7 @@ public class AppMainController {
                 new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/apps/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteAppMain(@PathVariable(name = "id")Long id) {
         final var result = appService.delete(id);
         return result ?
