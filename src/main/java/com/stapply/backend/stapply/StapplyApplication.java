23c85d271004efa42485df2329b075824a60dd31
Sw24sX <aleksandr.korolyov.99@mail.ru>
2021-05-12 11:24:28 +0500

    fix

--- src/main/java/com/stapply/backend/stapply/StapplyApplication.java
+++ src/main/java/com/stapply/backend/stapply/StapplyApplication.java
@@ -2,14 +2,20 @@ package com.stapply.backend.stapply;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class StapplyApplication {
+public class StapplyApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(StapplyApplication.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
--- src/main/java/com/stapply/backend/stapply/configurations/WebConfig.java
+++ src/main/java/com/stapply/backend/stapply/configurations/WebConfig.java
@@ -1,16 +1,55 @@
 package com.stapply.backend.stapply.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+import static springfox.documentation.builders.PathSelectors.regex;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-}
+//@Configuration
+//@EnableWebMvc
+//@EnableSwagger2
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
+//                .paths(regex("/product.*"))
+//                .build()
+//                .apiInfo(metaData());
+//    }
+//    private ApiInfo metaData() {
+//        return new ApiInfoBuilder()
+//                .title("Spring Boot REST API")
+//                .description("\"Spring Boot REST API for Online Store\"")
+//                .version("1.0.0")
+//                .license("Apache License Version 2.0")
+//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+//                .contact(new Contact("John Thompson", "http://springframework.guru/about/", "john@springfrmework.guru"))
+//                .build();
+//    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**");
+//    }
+//}
