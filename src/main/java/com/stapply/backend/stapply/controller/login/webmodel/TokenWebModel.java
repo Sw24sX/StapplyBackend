42ae078a71bb49729e3da9f9c702969d098d40b2
Sw24sX <aleksandr.korolyov.99@mail.ru>
2021-06-11 19:53:57 +0500

    fix'

--- src/main/java/com/stapply/backend/stapply/controller/login/webmodel/TokenWebModel.java
+++ src/main/java/com/stapply/backend/stapply/controller/login/webmodel/TokenWebModel.java
@@ -1,8 +1,8 @@
 package com.stapply.backend.stapply.controller.login.webmodel;
 
 public class TokenWebModel {
-    private final String username;
-    private final String token;
+    public final String username;
+    public final String token;
 
     public TokenWebModel(String username, String token) {
         this.username = username;
